b2f20ec882355e1843a0d7ae5b80cc33e7b17367
Jenny Yao <59626813+Jiaying-Yao@users.noreply.github.com>
2023-03-04 13:00:56 -0500

    Delete hw1 directory

--- hw1/submission_template/scripts/Untitled-1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.DataFrame({'a':[True, False,True],'b':['apple','banana','banana']})
-x,y=df[df['a']==True].shape
-#print(x)
-
-a=pd.DataFrame({'a':[1,2,3,4,5], 'b':[1,2,3,4,5]})
-print(a.iloc[:2,])
